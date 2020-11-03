3ee1ff281a0718b0ad33191e4fcc9a7fe621d03d
Rachel Cross <racross1@gmail.com>
2020-11-03 23:25:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,10 @@ def nyc_pigeon_organizer(data)
   row_index = 0 
   name_list = []
   while row_index < data.to_a.count do
-    binding.pry
     element_index = 0 
     while element_index < data.to_a[row_index].to_a.count do 
       name_list << data.to_a[row_index].to_a[element_index]
+      binding.pry
       element_index += 1 
     end 
     row_index += 1 
