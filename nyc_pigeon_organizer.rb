cbb41aabe15a0944dea59e2301c8fa3f41efc0d4
Rachel Cross <racross1@gmail.com>
2020-11-03 23:09:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
+  pigeon_list = {}
+  data
+  data.map do |
   # write your code here!
 end
+
+#want to iterate over HoHoA and pull out each array item which is a key
+#then match up each name key to HoH
+
+#first, iterate to find 
+
+#
