7cc68f4326efb54f847038b50c8c887ee5b9fc34
Rachel Cross <racross1@gmail.com>
2020-11-03 23:33:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  row_index = 0 
-  name_list = []
-  while row_index < data.to_a.count do
-    element_index = 0 
-    while element_index < data.to_a[row_index].to_a.count do 
-      name_list << data.to_a[row_index].to_a[element_index]
-      binding.pry
-      element_index += 1 
-    end 
-    row_index += 1 
-  end
-  p name_list
+  data.map do |item|
+  data[:color].to_a
 end 
  
  
