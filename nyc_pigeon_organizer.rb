9a54189e321690fd44990550809881c096349ad8
Rachel Cross <racross1@gmail.com>
2020-11-04 02:24:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
     pigeon_list.merge(array_of_keys[i]=> {:color => [], :gender => [], :lives => []})
   end 
     pigeon_list
-    binding.pry
+  binding.pry
 end 
 ### SAME PROCESS AS WITH NAME LIST BUT USE INCLUDE? AND KEY
 
