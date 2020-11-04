fe158e9fb03376213f1975e60fc913d3b0524042
Rachel Cross <racross1@gmail.com>
2020-11-04 00:34:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,6 +8,7 @@ def nyc_pigeon_organizer(data)
   array_of_categries = array_of_colors + array_of_genders + array_of_lives
   row_index = 0 
   while row_index < array_of_categries.count do 
+    element_index = 0
     
   
   
