768f761e0af45b03ac90863d3a4487dec8e29ff4
Rachel Cross <racross1@gmail.com>
2020-11-04 02:37:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,10 +7,11 @@ def nyc_pigeon_organizer(data)
   while i < array_of_keys do
     pigeon_list.store(array_of_keys[i]=> {:color => [], :gender => [], :lives => []})
   end 
-binding.pry
   pigeon_list
   #pigeon_list.each do |key, value|
    # if data[:color][]
+binding.pry
+
 end 
 
 ### SAME PROCESS AS WITH NAME LIST BUT USE INCLUDE? AND KEY
