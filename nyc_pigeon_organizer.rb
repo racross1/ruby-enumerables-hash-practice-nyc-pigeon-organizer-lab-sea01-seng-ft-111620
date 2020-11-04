139041dac1206c36772dcce3adcde6d7581c13e3
Rachel Cross <racross1@gmail.com>
2020-11-04 02:25:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   i = 0 
   while i < array_of_keys do
     pigeon_list.merge(array_of_keys[i]=> {:color => [], :gender => [], :lives => []})
-binding.pry
+    binding.pry
   end 
     pigeon_list
 end 
